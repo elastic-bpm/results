c886b5b1d34b6b944b0c35ff536ffaca3e5cf3e2
DJBnJack <johannes.bertens@gmail.com>
2016-12-27 17:46:27 +0100

    Even more functions!

--- rcode/graph.r
+++ rcode/graph.r
@@ -2,108 +2,97 @@ library(jsonlite)
 library(ggplot2)
 library( ReporteRs )
 
+saveMyPlot <- function(p, name) {
+  print(p)
+  dev.copy(png, paste(name,".png", sep=""))
+  dev.off()
+  Sys.sleep(0)
+  
+  print(p)
+  dev.copy(win.metafile, paste(name,".metafile", sep=""))
+  dev.off()
+  Sys.sleep(0)
+}
 
-#setwd("C:/Users/Johannes/Projects/results/output/20161218")
-#setwd("C:/Users/Johannes/Projects/results/output/20161218.03")
 setwd("C:/Users/Johannes/Projects/results/output/20161227")
-#logDF <- fromJSON("20161227-logstash.json", flatten = TRUE)
 
-#workerStart <- logDF[grep("worker:start", logDF$"_source.message", ignore.case=T),]
-#workerDone <- logDF[grep("worker:done", logDF$"_source.message", ignore.case=T),]
+logDF <- fromJSON("20161227-logstash.json", flatten = TRUE)
+logDF$timeEpoch <- as.numeric( logDF$`fields.@timestamp`)
+logDF$time <- as.POSIXct(logDF$timeEpoch/1000, origin="1970-01-01", tz="Europe/Amsterdam")
 
-#workerStart$start <- 1
-#workerStart$timeEpoch <- as.numeric( workerStart$`fields.@timestamp`)
-#workerStart$time <- as.POSIXct(workerStart$timeEpoch/1000, origin="1970-01-01", tz="Europe/Amsterdam")
+workerStart <- logDF[grep("worker:start", logDF$"_source.message", ignore.case=T),]
+workerDone <- logDF[grep("worker:done", logDF$"_source.message", ignore.case=T),]
 
-#workerDone$start <- -1
-#workerDone$timeEpoch <- as.numeric( workerDone$`fields.@timestamp`)
-#workerDone$time <- as.POSIXct(workerDone$timeEpoch/1000, origin="1970-01-01", tz="Europe/Amsterdam")
+workerStart$start <- 1
+workerDone$start <- -1
 
-#total <- rbind(workerStart, workerDone)
-
-#hist(workerStart$time, breaks="mins", freq=TRUE, format="%H:%M:%S")
-#hist(workerDone$time, breaks="mins", freq=TRUE, format="%H:%M:%S")
-
-#save(workerStart, file = "workerStart.RData")
-#save(workerDone, file = "workerDone.RData")
-#save(logDF, file = "logDF.RData")
+firstStart <- min(workerStart$timeEpoch)
 
 metricsDF <- fromJSON("20161227-metrics.json", flatten = TRUE)
 metricsDF <- metricsDF[grep("^node", metricsDF$`_source.beat.hostname`),]
 metricsDF$timeEpoch <- as.numeric( metricsDF$`fields.@timestamp`)
+metricsDF$timeFromStart <- (metricsDF$timeEpoch - firstStart)/1000
 metricsDF$time <- as.POSIXct(metricsDF$timeEpoch/1000, origin="1970-01-01", tz="Europe/Amsterdam")
 #summary(metricsDF)
-
 
 loadDF <- metricsDF[grep("load", metricsDF$"_source.metricset.name"),]
 #summary(loadDF)
 
-p <- ggplot(loadDF, aes(loadDF$time, loadDF$`_source.system.load.5`))
-p + geom_line(aes(colour = factor(loadDF$`_source.beat.hostname`)))
-dev.copy(png, "load.png")
-dev.off()
+plotLoad <- function(df) {
+  p <- ggplot(df, aes(df$timeFromStart, df$`_source.system.load.5`))
+  p <- p + geom_line(aes(colour = factor(df$`_source.beat.hostname`)))
+  p <- p + xlab("Time (in seconds) since first Workflow started") + ylab("System load over 5 minutes")     
+  p <- p + scale_colour_discrete(name="")
+  p <- p + theme(legend.position="top")
+  return (p)
+}
+saveMyPlot(plotLoad(loadDF), "load")
 
-p <- ggplot(loadDF, aes(loadDF$time, loadDF$`_source.system.load.5`))
-p + geom_line(aes(colour = factor(loadDF$`_source.beat.hostname`)))
-dev.copy(win.metafile,"load.metafile")
-dev.off()
 
 writeDF <- metricsDF[complete.cases(metricsDF$`_source.system.diskio.write.bytes`),]
 writeDF <- writeDF[grep("sda1", writeDF$`_source.system.diskio.name`),]
-#summary(writeDF)
-#writeDF
-
-p <- ggplot(writeDF, aes(writeDF$time, writeDF$`_source.system.diskio.write.bytes`))
-p + geom_line(aes(colour = factor(writeDF$`_source.beat.hostname`)))
-dev.copy(png, "disk.png")
-dev.off()
-
-p <- ggplot(writeDF, aes(writeDF$time, writeDF$`_source.system.diskio.write.bytes`))
-p + geom_line(aes(colour = factor(writeDF$`_source.beat.hostname`)))
-dev.copy(win.metafile,"disk.metafile")
-dev.off()
-
+#writeDF[writeDF$timeFromStart <= 10 & writeDF$timeFromStart > 0,]$`_source.beat.hostname`
+plotDisk <- function(df) {
+  p <- ggplot(df, aes(df$timeFromStart, df$`_source.system.diskio.write.bytes`))
+  p <- p + geom_line(aes(colour = factor(df$`_source.beat.hostname`)))
+  p <- p + xlab("Time (in seconds) since first Workflow started") + ylab("Bytes written to disk since start")     
+  p <- p + scale_colour_discrete(name="")
+  p <- p + theme(legend.position="top")
+  return (p)
+}
+saveMyPlot(plotDisk(writeDF), "disk")
 
 memDF <- metricsDF[complete.cases(metricsDF$`_source.system.memory.actual.used.pct`),]
-#summary(memDF)
-#memDF
-
-p <- ggplot(memDF, aes(memDF$time, memDF$`_source.system.memory.actual.used.pct`))
-p + geom_line(aes(colour = factor(memDF$`_source.beat.hostname`)))
-dev.copy(png, "mem.png")
-dev.off()
-
-p <- ggplot(memDF, aes(memDF$time, memDF$`_source.system.memory.actual.used.pct`))
-p + geom_line(aes(colour = factor(memDF$`_source.beat.hostname`)))
-dev.copy(win.metafile, "mem.metafile")
-dev.off()
-
+plotMemory <- function(df) {
+  p <- ggplot(df, aes(df$timeFromStart, df$`_source.system.memory.actual.used.pct`))
+  p <- p + geom_line(aes(colour = factor(df$`_source.beat.hostname`)))
+  p <- p + xlab("Time (in seconds) since first Workflow started") + ylab("Memory usage")     
+  p <- p + scale_colour_discrete(name="")
+  p <- p + theme(legend.position="top")
+  return (p)
+}
+saveMyPlot(plotMemory(memDF), "mem")
 
 cpuDF <- metricsDF[complete.cases(metricsDF$`_source.system.cpu.user.pct`),]
-#summary(cpuDF)
-#cpuDF
-
-p <- ggplot(cpuDF, aes(cpuDF$time, cpuDF$`_source.system.cpu.user.pct`))
-p + geom_line(aes(colour = factor(cpuDF$`_source.beat.hostname`)))
-dev.copy(png, "cpu.png")
-dev.off()
-
-p <- ggplot(cpuDF, aes(cpuDF$time, cpuDF$`_source.system.cpu.user.pct`))
-p + geom_line(aes(colour = factor(cpuDF$`_source.beat.hostname`)))
-dev.copy(win.metafile, "cpu.metafile")
-dev.off()
+plotCPU <- function(df) {
+  p <- ggplot(df, aes(df$timeFromStart, df$`_source.system.cpu.user.pct`))
+  p <- p + geom_line(aes(colour = factor(df$`_source.beat.hostname`)))
+  p <- p + xlab("Time (in seconds) since first Workflow started") + ylab("CPU usage")     
+  p <- p + scale_colour_discrete(name="")
+  p <- p + theme(legend.position="top")
+  return (p)
+}
+saveMyPlot(plotCPU(cpuDF), "cpu")
 
 netDF <- metricsDF[complete.cases(metricsDF$`_source.system.network.in.bytes`),]
 netDF <- netDF[grep("\\beth0\\b", netDF$`_source.system.network.name`),]
-#summary(netDF)
-#netDF
-
-p <- ggplot(netDF, aes(netDF$time, netDF$`_source.system.network.in.bytes`))
-p + geom_line(aes(colour = factor(netDF$`_source.beat.hostname`)))
-dev.copy(png, "net.png")
-dev.off()
-
-p <- ggplot(netDF, aes(netDF$time, netDF$`_source.system.network.in.bytes`))
-p + geom_line(aes(colour = factor(netDF$`_source.beat.hostname`)))
-dev.copy(win.metafile, "net.metafile")
-dev.off()
+netDF$MBs <- netDF$`_source.system.network.in.bytes` / 1024 / 1024 / 1024 #TO GB
+plotNet <- function(df) {
+  p <- ggplot(df, aes(df$timeFromStart, df$MBs))
+  p <- p + geom_line(aes(colour = factor(df$`_source.beat.hostname`)))
+  p <- p + xlab("Time (in seconds) since first Workflow started") + ylab("GBs received over eth0 since start")
+  p <- p + scale_colour_discrete(name="")
+  p <- p + theme(legend.position="top")
+  return (p)
+}
+saveMyPlot(plotNet(netDF), "net")
